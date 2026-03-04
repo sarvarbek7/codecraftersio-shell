77113701d7f6579589b9d000bc81604872021cf9
sarvarbek7 <bakhtiyorov.sarvar2001@gmail.com>
2026-03-04 11:25:31 +0500

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,6 +2,15 @@ class Program
 {
     static void Main()
     {
+        IReadOnlyList<string> validCommands = ["clear"];
+
         Console.Write("$ ");
+        
+        var input = Console.ReadLine();
+
+        if (!validCommands.Contains(input))
+        {
+            Console.WriteLine($"{input}: command not found");
+        }
     }
 }
